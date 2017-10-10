65f08749b5a004151d0baa145c72b2f5480441aa
JParmar13 <jay.parmar@hotmail.co.uk>
2017-10-10 20:18:08 +0100

    Add project files.

---
+++ h2ohelper/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace h2ohelper.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Calculate()
+        {
+            ViewBag.Message = "This page is where all the hydration finding comes out.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
