e78b4944f21fc745c5ddee3c58a1793b44069122
JParmar13 <jay.parmar@hotmail.co.uk>
2017-10-12 02:52:02 +0100

    Added login/register

---
+++ dbo.Table.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[UserID] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [UserName] NCHAR(50) NULL, 
+    [Password] NCHAR(100) NULL
+)
---
+++ h2ohelper/Models/OurDbContext.cs
@@ -0,0 +1,11 @@
+﻿using System.Data.Entity;
+
+namespace h2ohelper.Models
+{
+    public class OurDbContext : DbContext
+    {
+        public DbSet<UserAccount> userAccount { get; set; }
+
+
+    }
+}
