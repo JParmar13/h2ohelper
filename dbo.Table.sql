1a4e94a66ee7f874552d927e94c16a56887063c5
JParmar13 <jay.parmar@hotmail.co.uk>
2017-10-12 03:07:13 +0100

    Revert "Added login/register"
    This reverts commit e78b4944f21fc745c5ddee3c58a1793b44069122.

--- dbo.Table.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE TABLE [dbo].[Table]
-(
-	[UserID] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [UserName] NCHAR(50) NULL, 
-    [Password] NCHAR(100) NULL
-)
--- h2ohelper/App_Start/BundleConfig.cs
+++ h2ohelper/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace h2ohelper
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap-yeti.css",
+                      "~/Content/bootstrap.css",
                       "~/Content/site.css"));
         }
     }
