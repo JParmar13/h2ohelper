9bf4441a555881e29962359747c8ddf88d17d944
JParmar13 <jay.parmar@hotmail.co.uk>
2017-10-12 18:14:28 +0100

    Simple Login/Register, need to try asp identity

---
+++ h2ohelper/Models/UserAccount.cs
@@ -0,0 +1,30 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+namespace h2ohelper.Models
+{
+    public class UserAccount
+    {
+        [Key]
+        public int UserId { get; set; }
+        [Required(ErrorMessage = "First name is required")]
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = "Last is required")]
+        public string LastName { get; set; }
+        [Required(ErrorMessage = "Email is required")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Username is required")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Compare("Password", ErrorMessage = "Please confirm your password")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+    }
+}
